9429b8a098c1f21a97fc70139f90e0a955052064
rodrigorgs <rodrigorgs@cfbb319d-eb4e-0410-a72d-2b58981927c4>
2008-06-06 23:33:23 +0000

    Adicionando arquivos do Prolog (eu já atualizei depois da reunião).

---
+++ prolog/alunos.pl
@@ -0,0 +1,3 @@
+aluno(rodrigo, "Rodrigo Rocha").
+
+pre_matricula(rodrigo, fis122).
---
+++ prolog/turmas.pl
@@ -0,0 +1,239 @@
+turma(adm170030000, adm170, '030000').
+turma(adm170010000, adm170, '010000').
+turma(adm171020000, adm171, '020000').
+turma(adm226010100, adm226, '010100').
+turma(edc242030000, edc242, '030000').
+turma(fch162040000, fch162, '040000').
+turma(fch162141400, fch162, '141400').
+turma(fis121181800, fis121, '181800').
+turma(fis121181900, fis121, '181900').
+turma(fis121363700, fis121, '363700').
+turma(fis121040700, fis121, '040700').
+turma(fis122040800, fis122, '040800').
+turma(fis122010400, fis122, '010400').
+turma(ics003010000, ics003, '010000').
+turma(let042030000, let042, '030000').
+turma(let049010000, let049, '010000').
+turma(let051010000, let051, '010000').
+turma(let053010000, let053, '010000').
+turma(let359020000, let359, '020000').
+turma(let359010000, let359, '010000').
+turma(leta39010000, leta39, '010000').
+turma(leta41000100, leta41, '000100').
+turma(letb39020200, letb39, '020200').
+turma(mat007020200, mat007, '020200').
+turma(mat017040400, mat017, '040400').
+turma(mat017020200, mat017, '020200').
+turma(mat018020200, mat018, '020200').
+turma(mat042070700, mat042, '070700').
+turma(mat042090900, mat042, '090900').
+turma(mat043030300, mat043, '030300').
+turma(mat044010100, mat044, '010100').
+turma(mat046010100, mat046, '010100').
+turma(mat051020200, mat051, '020200').
+turma(mat051010100, mat051, '010100').
+turma(mat052010100, mat052, '010100').
+turma(mat053010100, mat053, '010100').
+turma(mat054010100, mat054, '010100').
+turma(mat055010100, mat055, '010100').
+turma(mat056010100, mat056, '010100').
+turma(mat057020200, mat057, '020200').
+turma(mat057010100, mat057, '010100').
+turma(mat061010100, mat061, '010100').
+turma(mat062010100, mat062, '010100').
+turma(mat112020200, mat112, '020200').
+turma(mat112010100, mat112, '010100').
+turma(mat144010100, mat144, '010100').
+turma(mat146010100, mat146, '010100').
+turma(mat147010100, mat147, '010100').
+turma(mat148010100, mat148, '010100').
+turma(mat149010100, mat149, '010100').
+turma(mat150010100, mat150, '010100').
+turma(mat151010100, mat151, '010100').
+turma(mat152010100, mat152, '010100').
+turma(mat153010100, mat153, '010100').
+turma(mat154010000, mat154, '010000').
+turma(mat155010000, mat155, '010000').
+turma(mat156010100, mat156, '010100').
+turma(mat157010100, mat157, '010100').
+turma(mat158010000, mat158, '010000').
+turma(mat159010000, mat159, '010000').
+turma(mat160010100, mat160, '010100').
+turma(mat161010100, mat161, '010100').
+turma(mat162010100, mat162, '010100').
+turma(mat163010100, mat163, '010100').
+turma(mat164010100, mat164, '010100').
+turma(mat165010100, mat165, '010100').
+turma(mat167010100, mat167, '010100').
+turma(mat169010100, mat169, '010100').
+turma(mat174040400, mat174, '040400').
+turma(mat191111100, mat191, '111100').
+turma(mat195010100, mat195, '010100').
+turma(mat208010100, mat208, '010100').
+turma(mat220020200, mat220, '020200').
+turma(mat220090000, mat220, '090000').
+turma(mata01010100, mata01, '010100').
+turma(mus128010100, mus128, '010100').
+turma(mus129010100, mus129, '010100').
+turma(qui037010100, qui037, '010100').
+
+horario(adm170010000, seg, 18, 20).
+horario(adm170010000, qua, 18, 20).
+horario(adm170030000, sex, 13, 17).
+horario(adm171010000, seg, 20, 22).
+horario(adm171010000, qua, 20, 22).
+horario(adm226020000, ter, 7, 9).
+horario(adm226020000, qui, 7, 9).
+horario(edc242010100, seg, 16, 19).
+horario(edc242010100, qua, 17, 19).
+horario(fch162030000, seg, 11, 13).
+horario(fch162030000, qua, 11, 13).
+horario(fch162040000, sex, 13, 17).
+horario(fis121141400, seg, 7, 9).
+horario(fis121141400, qua, 7, 9).
+horario(fis121141400, sex, 7, 9).
+horario(fis121181800, seg, 9, 11).
+horario(fis121181800, qua, 9, 11).
+horario(fis121181800, sex, 9, 11).
+horario(fis121181900, seg, 9, 11).
+horario(fis121181900, qua, 9, 11).
+horario(fis121181900, sex, 9, 11).
+horario(fis121363700, seg, 15, 17).
+horario(fis121363700, qua, 15, 17).
+horario(fis121363700, sex, 15, 17).
+horario(fis122040700, seg, 9, 11).
+horario(fis122040700, qua, 9, 11).
+horario(fis122040700, sex, 9, 11).
+horario(fis122040800, seg, 9, 11).
+horario(fis122040800, qua, 9, 11).
+horario(fis122040800, sex, 9, 11).
+horario(ics003010400, seg, 7, 9).
+horario(ics003010400, sex, 7, 12).
+horario(let042010000, sex, 7, 10).
+horario(let049030000, sex, 13, 16).
+horario(let051010000, seg, 17, 20).
+horario(let053010000, sex, 7, 10).
+horario(let359010000, sex, 7, 10).
+horario(let359020000, sex, 10, 13).
+horario(leta39010000, qua, 11, 13).
+horario(leta39010000, sex, 11, 13).
+horario(leta41010000, qua, 9, 11).
+horario(leta41010000, sex, 9, 11).
+horario(letb39000100, seg, 17, 19).
+horario(letb39000100, qua, 17, 19).
+horario(letb39000100, sex, 17, 19).
+horario(mat007020200, ter, 7, 9).
+horario(mat007020200, qui, 7, 9).
+horario(mat017020200, ter, 7, 9).
+horario(mat017020200, qui, 7, 9).
+horario(mat017040400, ter, 11, 13).
+horario(mat017040400, qui, 11, 13).
+horario(mat018020200, ter, 17, 19).
+horario(mat018020200, qui, 17, 19).
+horario(mat042020200, seg, 7, 9).
+horario(mat042020200, qua, 7, 9).
+horario(mat042020200, sex, 7, 9).
+horario(mat042070700, seg, 11, 13).
+horario(mat042070700, qua, 11, 13).
+horario(mat042070700, sex, 11, 13).
+horario(mat043090900, seg, 15, 17).
+horario(mat043090900, qua, 15, 17).
+horario(mat043090900, sex, 15, 17).
+horario(mat044030300, seg, 17, 19).
+horario(mat044030300, qua, 17, 19).
+horario(mat046010100, seg, 13, 15).
+horario(mat046010100, qua, 13, 15).
+horario(mat046010100, sex, 13, 15).
+horario(mat051010100, seg, 17, 19).
+horario(mat051010100, qua, 17, 19).
+horario(mat051020200, ter, 15, 17).
+horario(mat051020200, qui, 15, 17).
+horario(mat052010100, seg, 15, 17).
+horario(mat052010100, qua, 15, 17).
+horario(mat053010100, seg, 15, 17).
+horario(mat053010100, qua, 15, 17).
+horario(mat054010100, seg, 15, 17).
+horario(mat054010100, qua, 15, 17).
+horario(mat055010100, seg, 19, 21).
+horario(mat055010100, qua, 19, 21).
+horario(mat056010100, ter, 17, 19).
+horario(mat056010100, qua, 17, 19).
+horario(mat057010100, cmb, 0, 23).
+horario(mat057020200, cmb, 0, 23).
+horario(mat061010100, ter, 11, 13).
+horario(mat061010100, qui, 11, 13).
+horario(mat062010100, ter, 9, 11).
+horario(mat062010100, qui, 9, 11).
+horario(mat112010100, ter, 7, 10).
+horario(mat112010100, qui, 7, 10).
+horario(mat112020200, ter, 10, 13).
+horario(mat112020200, qui, 10, 13).
+horario(mat144010100, seg, 13, 15).
+horario(mat144010100, qua, 13, 15).
+horario(mat144010100, sex, 13, 15).
+horario(mat146010100, ter, 13, 15).
+horario(mat146010100, qui, 13, 15).
+horario(mat147010100, ter, 14, 17).
+horario(mat147010100, qui, 14, 17).
+horario(mat148010100, seg, 17, 19).
+horario(mat148010100, qua, 17, 19).
+horario(mat149010100, ter, 19, 21).
+horario(mat149010100, qui, 19, 21).
+horario(mat150010100, ter, 17, 19).
+horario(mat150010100, qui, 17, 19).
+horario(mat151010100, seg, 17, 19).
+horario(mat151010100, qua, 17, 19).
+horario(mat152010100, ter, 17, 19).
+horario(mat152010100, qui, 17, 19).
+horario(mat153010100, seg, 15, 17).
+horario(mat153010100, qua, 15, 17).
+horario(mat154010100, ter, 15, 17).
+horario(mat154010100, qui, 15, 17).
+horario(mat155010000, seg, 19, 21).
+horario(mat155010000, qua, 19, 21).
+horario(mat156010000, ter, 15, 17).
+horario(mat156010000, qui, 15, 17).
+horario(mat157010100, ter, 17, 19).
+horario(mat157010100, qui, 17, 19).
+horario(mat158010100, ter, 15, 17).
+horario(mat158010100, qui, 15, 17).
+horario(mat159010000, sex, 13, 16).
+horario(mat160010000, seg, 17, 19).
+horario(mat160010000, qua, 17, 19).
+horario(mat161010100, seg, 17, 19).
+horario(mat161010100, qua, 17, 19).
+horario(mat162010100, ter, 13, 15).
+horario(mat162010100, qui, 13, 15).
+horario(mat163010100, ter, 17, 19).
+horario(mat163010100, qui, 17, 19).
+horario(mat164010100, seg, 17, 19).
+horario(mat164010100, qua, 17, 19).
+horario(mat165010100, ter, 19, 21).
+horario(mat165010100, qui, 19, 21).
+horario(mat167010100, seg, 11, 13).
+horario(mat167010100, qua, 11, 13).
+horario(mat169010100, seg, 9, 11).
+horario(mat169010100, qua, 9, 11).
+horario(mat174010100, ter, 13, 15).
+horario(mat174010100, qui, 13, 15).
+horario(mat191040400, ter, 11, 13).
+horario(mat191040400, qui, 11, 13).
+horario(mat195111100, seg, 13, 15).
+horario(mat195111100, qua, 13, 15).
+horario(mat195111100, sex, 13, 15).
+horario(mat208010100, seg, 7, 9).
+horario(mat208010100, qua, 7, 9).
+horario(mat208010100, sex, 7, 9).
+horario(mat220010100, seg, 15, 17).
+horario(mat220010100, qua, 15, 17).
+horario(mat220020200, ter, 13, 15).
+horario(mat220020200, qui, 13, 15).
+horario(mata01090000, seg, 15, 17).
+horario(mata01090000, qua, 15, 17).
+horario(mus128010100, seg, 19, 21).
+horario(mus128010100, qua, 19, 22).
+horario(mus129010100, seg, 19, 21).
+horario(mus129010100, qua, 19, 22).
+horario(qui037010100, seg, 7, 11).
+horario(qui037010100, qua, 7, 9).
+horario(qui037010100, sex, 7, 9).
