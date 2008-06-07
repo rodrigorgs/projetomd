55d944f2bd905691afbaefdc670aaecf5cc6256a
rodrigorgs <rodrigorgs@cfbb319d-eb4e-0410-a72d-2b58981927c4>
2008-06-07 04:42:04 +0000

    Tornando dinâmicos os predicados que queremos modificar (através de asserts) na aplicação Java.

--- prolog/main.pl
+++ prolog/main.pl
@@ -1,10 +1,20 @@
-:- multifile 
-	eh_prerequisito/2,
-	horario/3,
+%:- multifile 
+%	eh_prerequisito/2,
+%	horario/3,
+%	turma/3,
+%	pre_matricula/3,
+%	pode_se_matricular_disc/3,
+%	matricula/2.
+
+:- dynamic 
+	disciplina/3,
+	semestre_sugerido/2,
+	eh_prerequisito_direto/2,
 	turma/3,
-	pre_matricula/3,
-	pode_se_matricular_disc/3,
-	matricula/2.
+	horario/4,
+	pre_matricula/3, 
+	matricula/2, 
+	foi_aprovado/3.
 
 :- ensure_loaded([disciplinas, turmas, alunos, regras]).
 
