5710c9fec1df9bccd82c3a9acce533f9cd653fba
rodrigorgs <rodrigorgs@cfbb319d-eb4e-0410-a72d-2b58981927c4>
2008-06-10 15:02:41 +0000

    Começando a integrar com o Prolog.

--- trunk/SPMP/src/java/spmp/business/prolog/regras.pl
+++ trunk/SPMP/src/java/spmp/business/prolog/regras.pl
@@ -1,3 +1,13 @@
+:- dynamic 
+	disciplina/3,
+	semestre_sugerido/2,
+	eh_prerequisito_direto/2,
+	turma/3,
+	horario/4,
+	pre_matricula/3, 
+	matricula/2, 
+	foi_aprovado/3.
+
 % As regras não consideram algumas situações:
 % - quebra de pré-requisito;
 % - aproveitamento de disciplinas (aluno transferido de outra universidade).
